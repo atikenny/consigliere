651f3b2f060d35cf1fb4b5f7e5969923db52a400
Attila Bartha <atikenny@gmail.com>
2017-08-12 21:44:43 +0100

    add tests for the parser service

---
+++ app/client/services/parser-service.spec.js
@@ -0,0 +1,71 @@
+import { parseCSV, formats, mapTransactions } from './parser-service';
+
+describe('parser-service', () => {
+    describe('parseCSV method', () => {
+        test('handles regular case', () => {
+            const CSV = 'head1,head2\nrow1-value1,row1-value2\nrow2-value1,row2-value2';
+            expect(parseCSV(CSV)).toEqual([
+                { head1: 'row1-value1', head2: 'row1-value2' },
+                { head1: 'row2-value1', head2: 'row2-value2' }
+            ]);
+        });
+
+        test('handles end of file new line case', () => {
+            const CSV = 'head1,head2\nrow1-value1,row1-value2\nrow2-value1,row2-value2\n';
+            expect(parseCSV(CSV)).toEqual([
+                { head1: 'row1-value1', head2: 'row1-value2' },
+                { head1: 'row2-value1', head2: 'row2-value2' }
+            ]);
+        });
+
+        test('handles additional comma at the enf of headers', () => {
+            const CSV = 'head1,head2,\nrow1-value1,row1-value2\nrow2-value1,row2-value2\n';
+            expect(parseCSV(CSV)).toEqual([
+                { head1: 'row1-value1', head2: 'row1-value2' },
+                { head1: 'row2-value1', head2: 'row2-value2' }
+            ]);
+        });
+    });
+
+    describe('formats', () => {
+        test('LLOYDS getDate converts a date to a date object', () => {
+            expect(formats.LLOYDS.getDate('01/02/2017')).toEqual(new Date('2017-02-01'));
+        });
+    });
+
+    describe('mapTransactions method', () => {
+        test('maps LLOYDS transactions', () => {
+            const lloydsFormat = formats.LLOYDS;
+            let debitTransaction = {};
+
+            debitTransaction[lloydsFormat.dateKey] = '01/02/2017';
+            debitTransaction[lloydsFormat.debitAmountKey] = '12.34';
+            debitTransaction[lloydsFormat.typeKey] = 'DEB';
+            debitTransaction[lloydsFormat.descriptionKey] = 'debit transaction description';
+
+            let creditTransaction = {};
+
+            creditTransaction[lloydsFormat.dateKey] = '31/12/2018';
+            creditTransaction[lloydsFormat.creditAmountKey] = '1234';
+            creditTransaction[lloydsFormat.typeKey] = 'DEP';
+            creditTransaction[lloydsFormat.descriptionKey] = 'credit transaction description';
+
+            const transactions = [debitTransaction, creditTransaction];
+
+            expect(mapTransactions(lloydsFormat, transactions)).toEqual([
+                {
+                    amount: 12.34,
+                    date: new Date('2017-02-01'),
+                    transactionType: 'debit',
+                    description: 'debit transaction description'
+                },
+                {
+                    amount: 1234,
+                    date: new Date('2018-12-31'),
+                    transactionType: 'credit',
+                    description: 'credit transaction description'
+                }
+            ]);
+        });
+    });
+});
