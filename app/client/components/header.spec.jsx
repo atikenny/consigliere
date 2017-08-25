48bda274d04f2d927e7c27a81cf3b91df9f1b9ce
Attila Bartha <atikenny@gmail.com>
2017-08-25 10:58:09 +0100

    test header component

---
+++ app/client/components/header.spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from './header';
+
+describe('Header component', () => {
+    test('has a header', () => {
+        const SUT = shallow(<Header />);
+        const header = SUT.find('header');
+
+        expect(header.length).toBe(1);
+    });
+
+    test('has an h1', () => {
+        const SUT = shallow(<Header />);
+        const heading = SUT.find('header h1');
+
+        expect(heading.text()).toBe('Consigliere');
+    });
+
+    test('has settings cog icon', () => {
+        const SUT = shallow(<Header />);
+        const cogIcon = SUT.find('.controllers IconButton');
+
+        expect(cogIcon.prop('type')).toBe('cog');
+    });
+});
